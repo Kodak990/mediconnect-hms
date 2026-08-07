abb45950193002c7ecbdadb40b201295dab79e45
Kodak990 <mayokunosanyinbi97@gmail.com>
2026-08-07 10:50:13 -0700

    Trust proxies for HTTPS behind Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
